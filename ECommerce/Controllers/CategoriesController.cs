7f73bade114e8779305a68f446018236796b8b3d
Yousef Ahmed <e255555z@gmail.com>
2025-04-15 09:50:57 +0200

    End Controller Part

---
+++ ECommerce/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+        public CategoriesController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Response =await _context.Categories.ToListAsync();
+            return View(Response);
+        }
+    }
+}
---
+++ ECommerce/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+        public ProductsController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var Response =await _context.Products.ToListAsync();
+            return View(Response);
+        }
+    }
+}
